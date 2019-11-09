34a474e3460dca6bea0e4567e87aa0712d24bea0
scottn12 <scott.norton12@gmail.com>
2019-11-09 16:40:05 -0500

    progress

--- back/server.py
+++ back/server.py
@@ -1,19 +1,25 @@
-from flask import Flask,jsonify, request
+from flask import Flask,jsonify, request, Response
 app = Flask(__name__)
 
-personState='no'
+personState = False
 
 @app.route('/checkForPerson')
 def checkForPerson():
     global personState
-    response = {'person':personState}
-    return jsonify(response)
+    resp = Response()
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    if personState:
+        return resp, 200
+    return resp, 404
 
 @app.route('/updatePersonState')
 def updateState():
     global personState
     state = request.args.get('person')
-    personState = state
+    if state == 'yes':
+        personState = True
+    else:
+        personState = False
     return 'success'
 
 if __name__ == '__main__':
---
+++ front/src/Components/Weather/Weather.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Weather.css'
+
+export default class Weather extends Component {
+
+  render() {
+    return(
+      <div>
+        weather
+      </div>
+    );
+  }
+
+
+
+};
