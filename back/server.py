637dd6a059a4a338149b4c5bf20716f8016de658
Jackson Lin <jackson1994y+git@gmail.com>
2019-11-09 16:03:15 -0500

    Dummy jsonresponse

--- back/server.py
+++ back/server.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 app.route('/checkForPerson')
 def checkForPerson():
-    pass
+    response = {'person':'no'}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run()
