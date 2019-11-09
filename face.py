eeb7bc0b253b1faa849f9802f33f4e8e0da8d799
Jackson Lin <jackson1994y+git@gmail.com>
2019-11-09 14:29:13 -0500

    Create facerecognition python

---
+++ face.py
@@ -0,0 +1,14 @@
+import asyncio
+import io
+import glob
+import os
+import sys
+import time
+import uuid
+import requests
+from urllib.parse import urlparse
+from io import BytesIO
+from PIL import Image, ImageDraw
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
